7c1b4e16721c4ffa5fdc6403d72fa39beab82f80
Learndevsec <rkumar72@lenovo.com>
2025-08-26 19:05:50 +0000

    Add new folders and files

---
+++ Day9/lab.py
@@ -0,0 +1,15 @@
+#Casting
+
+myScore = int(input("Your score: "))
+if myScore > 100000:
+  print("Winner!")
+else:
+  print("Try again 😭")
+
+#Float 
+
+myPi = float(input("What is Pi to 3dp? "))
+if myPi == 3.142 :
+  print("Exactly!")
+else:
+  print("Try again 😭")
