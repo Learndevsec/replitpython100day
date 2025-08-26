fd957587b4dfe416360fc9d42c42c87cb5679030
Learndevsec <rkumar72@lenovo.com>
2025-08-26 18:14:06 +0000

    Add new folders and files

---
+++ Day6/lab.py
@@ -0,0 +1,21 @@
+print("SECURE LOGIN")
+username = input("Username > ")
+
+if username == "mark":
+  print("Welcome Mark!")
+elif username == "suzanne":
+  print("Hey there Suzanne!")
+else:
+  print("Go away!")
+
+
+print("SECURE LOGIN")
+username = input("Username > ")
+password = input("Password> ")
+
+if username == "mark" and password == "password":
+ print("Welcome Mark!")
+elif username == "suzanne":
+ print("Hey there Suzanne!")
+else:
+ print("Go away!")
