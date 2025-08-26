e8446d7ddc02b2ed9f2684358b8997151afba3c9
Learndevsec <rkumar72@lenovo.com>
2025-08-26 17:13:18 +0000

    Initial commit

---
+++ Challenge/day5.py
@@ -0,0 +1,13 @@
+print("Marvel Movie Character Creator")
+print("--")
+hanging = input("Do you like hanging arourd?(yes/No): ")
+if hanging == "No":
+    print("Then you're not Spider-man")
+
+gravellyVoice = input("Do you have a 'gravelly' voice?: Yes/No")
+if gravellyVoice == "No":
+    print("Aww, then you're not Korg")
+
+feel = input("Do you often feel 'Marvelous'?: Yes/No")
+if feel == "Yes":
+    print("Aha! You're Captain Marvel! Hi!")
---
+++ common error/day5.py
@@ -0,0 +1,19 @@
+# Syntax Error
+catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+if catsOrDogs == "cat":
+  print("Meow")
+else:
+  print("Woof")
+
+  #Syntax Error...again
+catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+if catsOrDogs == "cat":
+  print("Meow")
+else:
+  print("Woof")
+
+  #catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+if catsOrDogs == "cat":
+  print("Meow")
+else:
+ print("Woof")
